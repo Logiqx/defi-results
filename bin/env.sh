b0ba4a5674f455f7e4f63ad254514b86099b5ebd
Logiqx <software@mikeg.me.uk>
2024-03-28 07:28:30 +0000

    Mount config

--- bin/env.sh
+++ bin/env.sh
@@ -5,6 +5,7 @@ WORK_DIR=/home/jovyan/work/$PROJ_NAME
 run_py_script()
 {
   docker run -it --rm \
+         --mount type=bind,src=$PROJ_DIR/config,dst=$WORK_DIR/config \
          --mount type=bind,src=$PROJ_DIR/data,dst=$WORK_DIR/data \
          --mount type=bind,src=$PROJ_DIR/docs,dst=$WORK_DIR/docs \
          ${PROJ_NAME}:${IMAGE_TAG:-latest} python/$1
